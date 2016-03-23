3e560d8c4f2baa748ffc141a79fc9957955ee930
user117310 <user117310@74-80-239-52.ic2net.net>
2016-03-23 11:12:59 -0700

    Added new methods to TakePhotoViewController.

--- AutoFinder/AutoFinder/AutoFinder/TakePhotoViewController.m
+++ AutoFinder/AutoFinder/AutoFinder/TakePhotoViewController.m
@@ -15,13 +15,54 @@
 
 @implementation TakePhotoViewController 
 
+-(void)viewDidLoad{
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+}
+
 
 -(IBAction)selectPhotoButton:(id)sender{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate=self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     
+    [self presentViewController:picker animated:YES
+                     completion:NULL];
 }
 
 -(IBAction)takePhotoButton:(id)sender{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate=self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
     
+    [self presentViewController:picker animated:YES
+                     completion:NULL];
+    
+}
+
+-(IBAction)sendPhoto:(id)sender{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:
+                          @"Thank you" message:@"The photo is selected" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Send", nil];
+    [alert show];
+}
+
+#pragma mark - Image Picker Controller delegate methods
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image=chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
