d196eea4b1af91c749a3f0ec791c86ecb1bed2d6
user117310 <user117310@74-80-239-52.ic2net.net>
2016-03-26 03:26:02 -0700

    FindDriverViewController modified.

--- AutoFinder/AutoFinder/AutoFinder/FindDriverViewController.m
+++ AutoFinder/AutoFinder/AutoFinder/FindDriverViewController.m
@@ -18,37 +18,55 @@
 
 - (IBAction)recieveNumber:(id)sender {
     
-    NSEntityDescription *entitydesc = [NSEntityDescription entityForName:@"User"
-                                                  inManagedObjectContext:context];
-    NSFetchRequest *request = [[NSFetchRequest alloc]init];
-    [request setEntity:entitydesc];
+    if ([self checkCarNumbeField]) {
+        NSEntityDescription *entitydesc = [NSEntityDescription entityForName:@"User"
+                                                      inManagedObjectContext:context];
+        NSFetchRequest *request = [[NSFetchRequest alloc]init];
+        [request setEntity:entitydesc];
     
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"car like %@", [[self carNumberField] text]];
-    [request setPredicate:predicate];
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"car like %@", [[self carNumberField] text]];
+        [request setPredicate:predicate];
     
-    NSError *error;
-    NSArray *matchingData=[context executeFetchRequest:request
+        NSError *error;
+        NSArray *matchingData=[context executeFetchRequest:request
                                                  error:&error];
     
-    if(matchingData.count <= 0){
-        UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"We are sorry"
-                                                       message:@"We have not found any number!"
-                                                      delegate:self
-                                             cancelButtonTitle:@"Dismiss"
-                                             otherButtonTitles:nil];
+        if(matchingData.count <= 0){
+            UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"We are sorry"
+                                                           message:@"We have not found any number!"
+                                                          delegate:self
+                                                 cancelButtonTitle:@"Dismiss"
+                                                 otherButtonTitles:nil];
         [alert show];
-    } else
-    {
-        NSString *phoneNumber;
+        } else {
+            NSString *phoneNumber;
         
-        for(NSManagedObjectContext *obj in matchingData) {
-            phoneNumber = [obj valueForKey:@"phone"];
-           
-        }
+            for(NSManagedObjectContext *obj in matchingData) {
+                phoneNumber = [obj valueForKey:@"phone"];
+            }
        
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"We found the  phone number" message:phoneNumber delegate:self cancelButtonTitle:@"cacel" otherButtonTitles:@"ok", nil];
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"We found the phone number" message:phoneNumber delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
+        
+            [alert show];
+        }
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning" message:@"Car number not found !" delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
         
         [alert show];
     }
+}
+
+- (BOOL)checkCarNumbeField {
+    if ([[self carNumberField] text] == nil) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    AppDelegate *appdelegate= [[UIApplication sharedApplication]delegate];
+    context = [appdelegate managedObjectContext];
 }
 @end
